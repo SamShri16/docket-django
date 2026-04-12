b61de8e5ef46a409b621632f40fba651e053ee03
SamShri16 <samarthshrivastava05@gmail.com>
2026-04-12 09:59:44 +0530

    Step 7: Added task creation from UI

--- tasks/views.py
+++ tasks/views.py
@@ -4,5 +4,17 @@ from .models import Task
 
 @login_required
 def home(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST.get('description', '')
+
+        Task.objects.create(
+            user=request.user,
+            title=title,
+            description=description
+        )
+
+        return redirect('home')
+
     tasks = Task.objects.filter(user=request.user)
     return render(request, 'home.html', {'tasks': tasks})
