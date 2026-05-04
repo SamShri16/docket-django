de6d8603db20aaf81ca00d7c5d6e6ada708abb11
SamShri16 <samarthshrivastava05@gmail.com>
2026-05-04 21:02:35 +0530

    implemented styled login & register UI

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('login/', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='logout'),
