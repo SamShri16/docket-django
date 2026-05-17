c82d6cc09eb2370e4f387c807c2642a19ec776b8
SamShri16 <samarthshrivastava05@gmail.com>
2026-05-17 11:10:57 +0530

    Stabilized v2 project

--- docket/urls.py
+++ docket/urls.py
@@ -1,17 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 from tasks.views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Landing Page
     path('', landing_page, name='landing'),
 
-    # Accounts
-    path('accounts/', include('accounts.urls')),
-
-    # Tasks
     path('', include('tasks.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+
     path('tasks/', views.task_list, name='task_list'),
+
     path('add-task/', views.add_task, name='add_task'),
 ]
